fed55523149abe4209dde0601ed7001a2b521b87
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-01 18:24:42 +0600

    14. find power of any number using for loop.java

---
+++ Basics/14. Write a program to find power of any number using for loop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter the exponent number: ");
+        int exponent = sc.nextInt();
+
+        int result = 1;
+        for (int i = 0; i <exponent; i++) {
+            result = result*base;
+        }
+        System.out.println("The power of " + base + "^" + exponent + " is = " + result);
+
+
+    }
+}

34b80e5d6513c8253d2077d7968bbc8f48412ade
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-02 10:21:39 +0600

    5. sort array elements.java

---
+++ Array/5. sort array elements.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Main{
+    public static void main(String[] args){
+       int [] arr = {1,5,3,2,9,7};
+
+        Arrays.sort(arr);
+        System.out.println("The sorted array is = "+ Arrays.toString(arr));
+    }
+}
